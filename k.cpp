b196b5f3bfa8130ebafdb4161e193d20d8dce1dd
ravalkevin <90209818+ravalkevin@users.noreply.github.com>
2023-04-22 20:20:49 +0530

    Create k.cpp

---
+++ k.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1e9+7
+#define ll long long int
+#define FOR(i, j) for (int(i) = 0; (i) < (j); (i)++)
+
+//KWIN CODE 
+
+void solve(){
+
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
